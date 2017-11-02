92055229e43c2d9e317f110b4a588f6bafe430ad
Rangel Ivanov <radito3@mail.bg>
2017-11-02 11:58:39 +0200

    Switch rest clients code

--- RestClientOne/src/org/elsys/rest/RestClient.java
+++ RestClientOne/src/org/elsys/rest/RestClient.java
@@ -1,18 +1,42 @@
 package org.elsys.rest;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.stream.Collectors;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 public class RestClient {
 
-	private static AtomicLong counter = new AtomicLong(0);
+	private final String USER_AGENT = "Mozilla/5.0";
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getResponse() {
-		return "Called " + counter.getAndIncrement() + " times";
+		String result = "Client Two response: ";
+		
+		try {
+			URL obj = new URL("https://servlettwo.cfapps.io/Servlet");
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+	 		con.setRequestMethod("GET");
+	 		con.setRequestProperty("User-Agent", USER_AGENT);
+	 		
+	 		InputStream in = con.getInputStream();
+
+	 		result += new BufferedReader(new InputStreamReader(in))
+	 				  .lines().collect(Collectors.joining("\n"));
+	 		
+	 		in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
--- RestClientTwo/src/org/elsys/rest/RestClient.java
+++ RestClientTwo/src/org/elsys/rest/RestClient.java
@@ -1,42 +1,18 @@
 package org.elsys.rest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicLong;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 public class RestClient {
-
-	private final String USER_AGENT = "Mozilla/5.0";
+	
+	private static AtomicLong counter = new AtomicLong(0);
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getResponse() {
-		String result = "Client Two response: ";
-		
-		try {
-			URL obj = new URL("https://servlettwo.cfapps.io/Servlet");
-			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-	 		con.setRequestMethod("GET");
-	 		con.setRequestProperty("User-Agent", USER_AGENT);
-	 		
-	 		InputStream in = con.getInputStream();
-
-	 		result += new BufferedReader(new InputStreamReader(in))
-	 				  .lines().collect(Collectors.joining("\n"));
-	 		
-	 		in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		return "Called " + counter.getAndIncrement() + " times";
 	}
 }
