99af394ecc74014eb836a4e339bb9158787c557c
Rangel Ivanov <radito3@mail.bg>
2017-11-05 16:18:21 +0200

    Remove comments in Go code

--- main.go
+++ main.go
@@ -84,16 +84,6 @@ func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 	}
 }
 
-// Main will be used to initialize the plugin process,
-// as well as any dependencies you might require for your plugin.
 func main() {
-	// Any initialization for your plugin can be handled here
-	//
-	// Note: to run the plugin.Start method, we pass in a pointer to the struct
-	// implementing the interface defined at "code.cloudfoundry.org/cli/plugin/plugin.go"
-	//
-	// Note: The plugin's main() method is invoked at install time to collect
-	// metadata. The plugin will exit 0 and the Run([]string) method will not be
-	// invoked.
 	plugin.Start(new(BasicPlugin))
 }
