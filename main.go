b067bee984985f5d4b1121dcd374c2112c8acccf
Rangel Ivanov <radito3@mail.bg>
2017-11-06 22:37:07 +0200

    Plugin code structure update

--- main.go
+++ main.go
@@ -5,53 +5,55 @@ import (
 	"net/http"
 	"code.cloudfoundry.org/cli/plugin"
 	"io/ioutil"
-	//os
+	"os"
+	"errors"
 )
 
 type ApmPlugin struct{}
 
-func (c *ApmPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+func getResponse(args []string) string, error {
+	//os.Getenv() is also an option for uri
+	resp, err := http.Get(args[2])
+	if err != nil {
+		return "", errors.New("Sevice error on Get: ", err)
+	}
+	defer resp.Body.Close()
 
+	bs, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		return "", errors.New("Sevice error on Read: ", readErr)
+	}
+	return string(bs)
+}
+
+func (c *ApmPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] != "apm" {
-		fmt.Println("Incorrect usage.\nCorrect usage: cf apm <command>")
+		fmt.Println("Incorrect usage.\nCorrect usage: cf apm <command> [<app_name>]")
 		os.Exit(1)
 	}
-
 	switch {
 		case args[1] == "test-app":
-			//uri could be from os.Getenv()
-			resp, err := http.Get(args[2])
-		
+			resp, err := getResponse(args)
 			if err != nil {
-				fmt.Println("Sevice error on Get: ", err)
-				return
+				fmt.Println(err)
+			} else {
+				fmt.Println("Service One Response: ", resp)
 			}
-		
-			defer resp.Body.Close()
-		
-			bs, readErr := ioutil.ReadAll(resp.Body)
-		
-			if readErr != nil {
-				fmt.Println("Sevice error on Read: ", readErr)
-				return
-			}
-		
-			fmt.Println("Service One Response: ", string(bs))
 
 		case args[1] == "list-apps":
 			//list available apps from repo
 
 		case args[1] == "install":
-			//check for correct app name
-			fmt.Println("Incorrect application name.")
+			//check if such an app exists
+			fmt.Println("Application %s not found.", args[2])
 
 		case args[1] == "update":
-			//check for correct app name
-			fmt.Println("Incorrect application name.")
+			//check if such an app exists
+			fmt.Println("Application %s not found.", args[2])
 
 		case args[1] == "delete":
-			//check for correct app name
-			fmt.Println("Incorrect application name.")
+			//check if such an app exists
+			fmt.Println("Application %s not found.", args[2])
 
 		default:
 			fmt.Println("Incorrect command.\nCommands are install/update/delete <app_name>")
@@ -63,9 +65,9 @@ func (c *ApmPlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata {
 		Name: "apmPlugin",
 		Version: plugin.VersionType {
-			Major: 1,
-			Minor: 1,
-			Build: 2,
+			Major: 2,
+			Minor: 0,
+			Build: 0,
 		},
 		MinCliVersion: plugin.VersionType {
 			Major: 6,
