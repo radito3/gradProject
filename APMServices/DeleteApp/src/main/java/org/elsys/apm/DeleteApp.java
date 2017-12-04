3ca6d0afe9ba64e281d6a2319990ce168618a19a
Rangel Ivanov <radito3@mail.bg>
2017-12-04 15:25:46 +0200

    Add working deleteApp impl

--- APMServices/DeleteApp/src/main/java/org/elsys/apm/DeleteApp.java
+++ APMServices/DeleteApp/src/main/java/org/elsys/apm/DeleteApp.java
@@ -4,6 +4,8 @@ import org.cloudfoundry.client.lib.CloudCredentials;
 import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.CloudFoundryException;
 import org.cloudfoundry.client.lib.domain.*;
+import org.cloudfoundry.client.lib.rest.CloudControllerClient;
+import org.cloudfoundry.client.lib.rest.CloudControllerClientFactory;
 import org.springframework.http.HttpStatus;
 
 import javax.ws.rs.GET;
@@ -21,6 +23,8 @@ public class DeleteApp {
     private static final String user = System.getenv("user");
     private static final String password = System.getenv("pass");
     private static final String target = "https://api.run.pivotal.io";
+    private static final String orgName = "graduationProject.org";
+    private static final String spaceName = "development";
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -28,17 +32,21 @@ public class DeleteApp {
         StringBuilder result = new StringBuilder();
 
         try {
-            CloudFoundryClient client = new CloudFoundryClient(
+            CloudControllerClientFactory cl = new CloudControllerClientFactory(null, true);
+            CloudControllerClient client = cl.newCloudController(new URL(target), new CloudCredentials(user, password), orgName, spaceName);
+            client.getSpaces().forEach(s -> System.err.println(s));
+            /*CloudFoundryClient client = new CloudFoundryClient(
                     new CloudCredentials(user, password),
-                    new URL(target),
+                    new URL(target));,
                     new CloudSpace(CloudEntity.Meta.defaultMeta(), "development",
-                            new CloudOrganization(CloudEntity.Meta.defaultMeta(), "graduationProject.org")));
+                            new CloudOrganization(CloudEntity.Meta.defaultMeta(), "graduationProject.org")));*/
             client.login();
+            System.err.println("quering applications...");
+            List<CloudApplication> apps = client.getApplications();
+            apps.forEach(e -> System.err.println(e.getName()));
+            CloudApplication app = client.getApplication(appName);
+            System.err.print("got app..."+app.toString());
 
-//            List<CloudApplication> apps = client.getApplications();
-//            apps.forEach(e -> System.out.println(e.getName()));
-
-//            CloudApplication app = client.getApplication(appName);
             client.deleteApplication(appName);
             client.logout();
             result.append("App deleted.");
