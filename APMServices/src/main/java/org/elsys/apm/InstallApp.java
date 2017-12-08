01b3f776bbb3c0294fa947cfcda7f768175002f1
Rangel Ivanov <radito3@mail.bg>
2017-12-08 10:27:08 +0200

    Update InstallApp (tested & working)

--- APMServices/src/main/java/org/elsys/apm/InstallApp.java
+++ APMServices/src/main/java/org/elsys/apm/InstallApp.java
@@ -1,23 +1,23 @@
 package org.elsys.apm;
 
 import org.cloudfoundry.client.lib.UploadStatusCallback;
+import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.cloudfoundry.client.lib.domain.Staging;
 import org.cloudfoundry.client.lib.rest.CloudControllerClient;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+import org.modeshape.common.text.Inflector;
 
 import javax.net.ssl.HttpsURLConnection;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Objects;
 
 @Path("/{org}/{space}/install/{appName}")
 public class InstallApp {
@@ -28,12 +28,14 @@ public class InstallApp {
     @PathParam("space")
     private String spaceName;
 
-    private CloudControllerClient client = new CloudControllerClientProvider(orgName, spaceName, "").getClient();
-    private static final String buildpackUrl = "https://github.com/cloudfoundry/java-buildpack.git";
+    private CloudControllerClient client;
+
+    private static final String buildpackUrl = Buildpacks.JAVA.getUrl();
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String getInstallResult(@PathParam("appName") String appName) {
+    public String getInstallResult(@HeaderParam("access-token") String token, @PathParam("appName") String appName) {
+        client = new CloudControllerClientProvider(orgName, spaceName, token).getClient();
         StringBuilder result = new StringBuilder();
         StringBuilder staticAppUrl = new StringBuilder(CloudControllerClientProvider.getStaticAppUrl());
         try {
@@ -45,9 +47,8 @@ public class InstallApp {
             }
 
             JSONArray files = (JSONArray) app.get("files");
-            Iterator it = files.iterator();
-            while (it.hasNext()) {
-                String file = String.valueOf(it.next());
+            for (Object file1 : files) {
+                String file = String.valueOf(file1);
                 staticAppUrl.replace(staticAppUrl.lastIndexOf("/") + 1,
                         staticAppUrl.length(),
                         file);
@@ -68,25 +69,30 @@ public class InstallApp {
             HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
             InputStream in = con.getInputStream();
 
-            client.createApplication(appName,
+            String nameToUpload = Objects.equals(appName.toLowerCase(), fileName.split("-")[0].toLowerCase()) ?
+                    appName : fileName.split("-")[0];
+
+            String name = Inflector.getInstance().lowerCamelCase(nameToUpload);
+
+            client.createApplication(name,
                     new Staging("--no-manifest", buildpackUrl),
                     1000,  //disk space
                     1000,  //memory
                     Collections.singletonList("https://" + appName.toLowerCase() + ".cfapps.io"),
                     null);  //service names
 
-//            CloudApplication app = client.getApplication(appName);
-//            HashMap<Object, Object> ver = new HashMap<>();
-//            ver.put("version", "1.0.0");
-//            app.setEnv(ver);
+            client.uploadApplication(name, fileName, in, UploadStatusCallback.NONE);
 
-            client.uploadApplication(appName, fileName, in, UploadStatusCallback.NONE);
+            CloudApplication app = client.getApplication(name);
+            HashMap<Object, Object> ver = new HashMap<>();
+            ver.put("version", "1.0.0");
+            app.setEnv(ver);
 
             in.close();
         } catch (IOException e) {
             return "Error installing app";
         }
 
-        return "App installed successfully";
+        return "App installed successfully\n";
     }
-}
+ }
