b29c916d88ba1af86a90c06e37c7fcceea99042f
Rangel Ivanov <radito3@mail.bg>
2018-02-25 11:46:57 +0200

    Add tests for ApplicationUploader

--- APMServices/src/tests/java/org/elsys/apm/ApplicationUploaderTest.java
+++ APMServices/src/tests/java/org/elsys/apm/ApplicationUploaderTest.java
@@ -1,12 +1,30 @@
 package org.elsys.apm;
 
-import org.junit.Before;
+import org.cloudfoundry.client.lib.domain.Staging;
+import org.elsys.apm.model.CloudApp;
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.MissingResourceException;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class ApplicationUploaderTest {
@@ -14,43 +32,103 @@ public class ApplicationUploaderTest {
     @Mock
     private CloudClient client;
 
-    @Before
-    public void setUp() {
+    @Mock
+    private CloudApp app;
 
-    }
+    private ApplicationUploader uploader;
 
     @Test
     public void correctLanguageTest() {
+        ApplicationUploader.checkLanguage("java");
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void unsupportedLanguageTest() {
+        ApplicationUploader.checkLanguage("Unsupported language");
     }
 
     @Test
-    public void unsupportedLanguageTest() {
-
+    public void correctExceptionMessageOnLanguageTest() {
+        try {
+            ApplicationUploader.checkLanguage("Unsupported language");
+            fail("An exception should have been thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Unsupported language", e.getMessage());
+        }
     }
 
     @Test(expected = ApplicationUploader.ExistingAppException.class)
-    public void existingAppTest() {
+    public void existingAppTest() throws ParseException, IOException, ClassNotFoundException {
+        when(app.getName()).thenReturn("name");
+        when(client.checkForExistingApp(eq("name"))).thenReturn(true);
 
+        uploader = new ApplicationUploader(client);
+
+        uploader.install(app, 1, 1);
     }
 
     @Test
-    public void installWithNoDependenciesTest() {
+    public void correctExceptionMessageOnAppTest() throws ParseException, IOException, ClassNotFoundException {
+        when(app.getName()).thenReturn("name");
+        when(client.checkForExistingApp(eq("name"))).thenReturn(true);
 
+        uploader = new ApplicationUploader(client);
+
+        try {
+            uploader.install(app, 1, 1);
+            fail("An exception should have been thrown");
+        } catch (ApplicationUploader.ExistingAppException e) {
+            assertEquals("App already exists", e.getMessage());
+        }
     }
 
-    @Test
-    public void installWithDependenciesTest() {
+   private void prepareForInstall() throws IOException {
+       when(app.getName()).thenReturn("name");
+       when(app.getLanguage()).thenReturn("java");
+       when(app.hasDependencies()).thenReturn(false);
+       when(app.getFileName()).thenReturn("fileName");
 
-    }
+//        URL url = getMockedUrl();
 
-    @Test
-    public void checkInstalledDependenciesTest() {
+       when(app.getFileUrl()).thenReturn(getMockedUrl());
 
-    }
+       when(client.checkForExistingApp("name")).thenReturn(false);
+       doNothing().when(client).createApp("name", new Staging(), 1, 1, new ArrayList<>());
 
-    @Test(expected = MissingResourceException.class)
-    public void chceckMissingDependenciesTest() {
+       doNothing().when(client).uploadApp("name", "fileName",
+               new ByteArrayInputStream("<myList></myList>".getBytes("UTF-8")));
+   }
 
-    }
+   private URL getMockedUrl() throws IOException {
+       HttpsURLConnection mockUrlCon = mock(HttpsURLConnection.class);
+
+       ByteArrayInputStream is = new ByteArrayInputStream("<myList></myList>".getBytes("UTF-8"));
+       when(mockUrlCon.getInputStream()).thenReturn(is);
+
+       when(mockUrlCon.getLastModified()).thenReturn(10L, 11L);
+
+       URLStreamHandler stubUrlHandler = new URLStreamHandler() {
+           @Override
+           protected URLConnection openConnection(URL u) throws IOException {
+               return mockUrlCon;
+           }
+       };
+
+       return new URL("foo", "bar", 99, "/foobar", stubUrlHandler);
+   }
+
+   @Test
+   public void installWithNoDependenciesTest() throws ParseException, IOException, ClassNotFoundException {
+       prepareForInstall();
+
+       uploader = new ApplicationUploader(client);
+
+       uploader.install(app, 1, 1);
+   }
+
+   @Test
+   public void installWithDependenciesTest() {
+
+   }
+
 }
